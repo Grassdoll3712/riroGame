0c7ecc5bb0cfd02954756c1ddefa31cc7bd2c51f
Grassdoll3712 <cubejoon37@naver.com>
2024-09-20 15:21:04 +0900

    Bug fix

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
+const userName = document.querySelector('#container > div > div.treeWrap > div.user > div.only_pc.user_inner.clearfix > div.inner_top > div.left > div > div > div').innerHTML;
+async function sha256(message) {
+  const msgBuffer = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+const userHash = await sha256(userName);
+if (userHash != '557e23c37e80e113ad388f63fd8535037c9f73dbb8f3fcf9ee66012d80f24087') {
+  history.go(-1);
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 const level = urlParams.get('level')??0;
 const gameArea = document.getElementById('rText');
@@ -14,4 +27,4 @@ document.getElementById('gameSubmit').addEventListener('click', function () {
   window.location.href = url.href;
 });
 
-//<img id="init" src="https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png" style="width:0px;height:0px;" onload  ="const userName = document.querySelector('#container > div > div.treeWrap > div.user > div.only_pc.user_inner.clearfix > div.inner_top > div.left > div > div > div').innerHTML; async function sha256(message) { const msgBuffer = new TextEncoder().encode(message); const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer); const hashArray = Array.from(new Uint8Array(hashBuffer)); const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); return hashHex; } await sha256(userName) != '557e23c37e80e113ad388f63fd8535037c9f73dbb8f3fcf9ee66012d80f24087' ? history.go(-1):null; document.getElementById('init').remove(); const gameScript = document.createElement('script'); gameScript.src = 'https://grassdoll3712.github.io/riroGame/game.js'; const gameCSS = document.createElement('link'); gameCSS.rel = 'stylesheet'; gameCSS.href = 'https://grassdoll3712.github.io/riroGame/game.css'; document.head.appendChild(gameScript); document.head.appendChild(gameCSS);">
+//<img id="init" src="https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png" style="width:0px;height:0px;" onload  ="document.getElementById('init').remove(); const gameScript = document.createElement('script'); gameScript.src = 'https://grassdoll3712.github.io/riroGame/game.js'; const gameCSS = document.createElement('link'); gameCSS.rel = 'stylesheet'; gameCSS.href = 'https://grassdoll3712.github.io/riroGame/game.css'; document.head.appendChild(gameScript); document.head.appendChild(gameCSS);">
